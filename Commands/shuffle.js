0c2752420a2d2c471f3d48969a470730b03b516d
Proximitynow19 <55227471+Proximitynow19@users.noreply.github.com>
2020-04-17 17:20:57 +1200

    Added shuffle.js

---
+++ Commands/shuffle.js
@@ -0,0 +1,44 @@
+//
+// Copyright [2020] [Pulse]
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+const { RichEmbed } = require("discord.js");
+const { Utils } = require("erela.js");
+
+module.exports = {
+  name: "shuffle",
+  aliases: [],
+  usage: "",
+  description: "Shuffles the queue.",
+  needperms: [],
+  permissions: [],
+  async execute(message, args, client) {
+    const { voiceChannel } = message.member;
+    const player = client.music.players.get(message.guild.id);
+
+    if (!player)
+      return message.channel.send(
+        "No song/s currrently playing in this guild."
+      );
+
+    if (!voiceChannel || voiceChannel.id !== player.voiceChannel.id)
+      return message.channel.send(
+        "You need to be in a voice channel to shuffle music."
+      );
+
+    player.queue.shuffle();
+    return message.channel.send("The queue is now shuffled.");
+  },
+};
